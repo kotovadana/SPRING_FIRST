b45d1577ef8cb3d8701c30e27f0246246458239a
Kotova Dana <44918208+kotovadana@users.noreply.github.com>
2019-05-12 17:48:46 +0300

    created first endpoint with annotation @ResponseBody

---
+++ spring-boot/src/main/java/lv/sda/spring/springboot/HelloWorldController.java
@@ -0,0 +1,18 @@
+package lv.sda.spring.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/endpoint")
+    @ResponseBody
+    String exampleMethod() {
+        return "Hello There...So the Spring s.hould understand this as a reference" +
+                "to html file in 'template' folder, but because of annotation" +
+                "'@ResponseBody' - Spring returns only this String";
+    }
+}
