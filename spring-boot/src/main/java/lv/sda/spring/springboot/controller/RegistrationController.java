b39baed4882e2d20e3c81d4b084b62e235de06be
Kotova Dana <44918208+kotovadana@users.noreply.github.com>
2019-05-14 19:22:50 +0300

    added Model and some attributes

--- spring-boot/src/main/java/lv/sda/spring/springboot/controller/RegistrationController.java
+++ spring-boot/src/main/java/lv/sda/spring/springboot/controller/RegistrationController.java
@@ -1,7 +1,9 @@
 package lv.sda.spring.springboot.controller;
 
 
+import lv.sda.spring.springboot.entity.RegistrationFields;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +14,15 @@ public class RegistrationController {
     //This class will contain the methods responsible for handling incoming web requests
 
     @GetMapping("/registration") // = resources/templates/registration
-    public String getRegistrationInfo() {
+    public String getRegistrationInfo(Model model) {
+        model.addAttribute("registration_info", new RegistrationFields());
         return "registration/registration_form";
     }
 
     @PostMapping ("/registration")
-    public String submitRegistrationInfo (){
+    public String submitRegistrationInfo (Model model){
+        model.addAttribute("registration_info", new RegistrationFields());
+        model.addAttribute("submitted", true);
         return "registration/registration_form";
     }
 
