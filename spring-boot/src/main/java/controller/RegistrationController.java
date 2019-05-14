cdd843c0d7d12259e81879a5396022c8209c05ed
Kotova Dana <44918208+kotovadana@users.noreply.github.com>
2019-05-14 18:06:49 +0300

    Creating Controllew + GetMapping & PostMapping

---
+++ spring-boot/src/main/java/controller/RegistrationController.java
@@ -0,0 +1,24 @@
+package controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class RegistrationController {
+    //This class will contain the methods responsible for handling incoming web requests
+
+    @GetMapping ("/registration")
+    @ResponseBody
+    public String getRegistrationInfo() {
+        return "firstpage/registration_form";
+    }
+
+    @PostMapping ("/registration")
+    @ResponseBody
+    public String submitRegistrationInfo (){
+        return "firstpage/registration_form";
+    }
+}
