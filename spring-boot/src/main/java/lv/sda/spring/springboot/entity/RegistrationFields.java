b2d36c125dda4b706b990e82c5195c1dfc2b325c
Kotova Dana <44918208+kotovadana@users.noreply.github.com>
2019-05-14 18:47:44 +0300

    Registration fields are made

---
+++ spring-boot/src/main/java/lv/sda/spring/springboot/entity/RegistrationFields.java
@@ -0,0 +1,110 @@
+package lv.sda.spring.springboot.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class RegistrationFields {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String last_name;
+    private String username;
+    private long age;
+    private long weight;
+    private long height;
+    private String email;
+    private Date created;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public long getAge() {
+        return age;
+    }
+
+    public void setAge(long age) {
+        this.age = age;
+    }
+
+    public long getWeight() {
+        return weight;
+    }
+
+    public void setWeight(long weight) {
+        this.weight = weight;
+    }
+
+    public long getHeight() {
+        return height;
+    }
+
+    public void setHeight(long height) {
+        this.height = height;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+
+    private Date updated;
+
+    public RegistrationFields () {}
+
+
+
+}
